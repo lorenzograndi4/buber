579c01f4237812774deed544b95d001d0e1d7bc8
lorenzograndi4 <lorenzo.grandi4@gmail.com>
2017-06-29 17:22:48 +0200

    temp version

--- buber.rb
+++ buber.rb
@@ -1,26 +1,33 @@
 require_relative 'passenger'
 
-class Buber
-  attr_accessor :taxis, :passengers
-  def initialize
-    @taxis = 20
-    @passengers = []
-#    has_taxis * 20
-    puts "We got #{@taxis} working vehicles!"
-  end
-
-  # def has_taxis
-  #   @taxis += 1
-  #   #taxis.belong_to(self)
-  # end
-end
-
 class SpaceTaxi
-  attr_accessor
-  def initialize
+  attr_accessor :id, :availability
+  def initialize(id, availability = true)
+    @id = id
+    @availability = availability
   end
-
   # def belong_to(company)
   #   @company = company
   # end
+end
+
+class Buber
+  attr_accessor :taxis, :passengers
+  def initialize(taxis = [])
+    @taxis = taxis
+    @passengers = []
+    puts "We got #{@taxis.count} working vehicles!"
+  end
+
+  def check_taxis
+    while @taxis.count <= 20
+      new_taxi = SpaceTaxi.new(rand(1..999))
+      @taxis << new_taxi
+    end
+  end
+
+  def print_taxis
+    @taxis.each {|t| puts "#{t.id} is available: #{t.availability}."}
+  end
+
 end
--- main.rb
+++ main.rb
@@ -1,5 +1,9 @@
 require_relative 'buber'
 require_relative 'passenger'
 
-Buber.new#.run
+a_taxi = SpaceTaxi.new(rand(1..999))
+test = Buber.new#.run
 Passenger.new
+
+test.check_taxis
+test.print_taxis
